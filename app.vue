df20d92e10a92318ef8148f021726867e347a9b3
Vinicius Inocêncio <vicnicius2009@gmail.com>
2023-11-14 21:28:35 -0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <HomeComponent/>
+  </div>
+</template>
+
+<style lang="scss">
+@use '~/styles/global.scss'
+</style>
+
+<script setup lang="ts">
+
+useHead({
+  title: 'Inocêncio\'s portifolio',
+  meta: [{
+    name: 'color-scheme',
+    content: 'dark light'
+  }],
+})
+</script>
